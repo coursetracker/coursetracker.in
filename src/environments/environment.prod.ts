6f72c511aebb8e7ea64467b1ae83678b6bcc5e94
Naresh Kumar H <nareshkumarh@live.com>
2021-03-10 17:38:42 +0530

    Added theme environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,4 +6,17 @@ export const environment = {
     'https://10bd66a9-2a88-4ae9-8854-849290d1bb9e-bluemix.cloudantnosqldb.appdomain.cloud/',
   DB_USERNAME: 'apikey-v2-1ni2bnyzfoegwbtpx3lsitoqwkne4mmgh64r2g5m6x77',
   DB_PASSWORD: '82da6ad6e7071bc68f7c1df3c48048ad',
+  theme: {
+    bg1Color: '#f2f2f2',
+    bgColor: 'cadetblue',
+    orgId: 'spinsoft',
+    logo:
+      'https://s3.ap-south-1.amazonaws.com/assets.coursetracker.in/spinsoft_logo.png',
+    bg1Image:
+      "url('https://s3.ap-south-1.amazonaws.com/assets.coursetracker.in/blue.jpg')",
+    orgName: 'CourseTracker',
+    displayOrgName: true,
+    authBoxLeftColor: 'white',
+    authBoxRightColor: 'white',
+  },
 };
